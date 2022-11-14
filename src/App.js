49cb204b70427f6849ee179401f22bb5a8120bf0
Austin King <atkingtornado@gmail.com>
2022-11-14 12:11:33 -0700

    added comparison toggle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styles from './app.module.css';
+
+import Switch from "react-switch";
 
 import ImageDisplay from './features/ImageDisplay'
 
 function App() {
+  const [comparisonToggled, setComparisonToggled] = useState(false);
+
+  const handleToggleComparison = () => {
+    setComparisonToggled(!comparisonToggled)
+  }
+
   return (
     <div className="App">
       <div>
-        <h1>WPC ERO Verification</h1>
-        <ImageDisplay/>
+        <h1 className={styles.TitleHeader}>WPC ERO Verification</h1>
+        <div className={styles.ComparisonToggleContainer}>
+            <p className={styles.ComparisonToggleLabel}>Enable Comparison:</p>
+            <Switch className={styles.ComparisonToggleSwitch} onChange={handleToggleComparison} checked={comparisonToggled} />
+        </div>
+
+
+        <div className={comparisonToggled ? styles.MultiPlotContainer : styles.SinglePlotContainer}>
+          <ImageDisplay/>
+        </div>
+        { comparisonToggled ? 
+          <div className={styles.MultiPlotContainer}>
+            <ImageDisplay/>
+          </div>
+          :
+          null
+        }
+
+
       </div>
     </div>
   );
