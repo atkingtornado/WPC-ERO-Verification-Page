3dca0767784f1c8a247a6f44a95c43a499d2e03f
Austin King <atkingtornado@gmail.com>
2022-12-01 12:51:07 -0700

    finished archived plot display

--- src/features/ImageDisplay.jsx
+++ src/features/ImageDisplay.jsx
@@ -12,39 +12,65 @@ const options = [
   {
     label: 'Most Recent ERO Verification Day',
     options: [
-      { value: 'ALL_EROwST4gFFG_last_v', label: 'ERO With Verification Overlay' },
-      { value: 'ALL_ST4gFFG_last_v', label: 'Verification Only' },
-      { value: 'ALL_EROwALL_PP_last_v', label: 'Practically Perfect Verification' },
+      { value: 'ALL_EROwST4gFFG', label: 'ERO With Verification Overlay' },
+      { value: 'ALL_ST4gFFG', label: 'Verification Only' },
+      { value: 'ALL_EROwALL_PP', label: 'Practically Perfect Verification' },
     ]
   },
   {
     label: 'CSU-MLP Daily Verification',
     options: [
-      { value: 'ALL_CSUopUFVSv2022_last_v', label: 'CSU-MLP GEFS Operational - 2022 UFVS Version' },
-      { value: 'ALL_CSUopv2022_last_v', label: 'CSU-MLP GEFS Operational - 2022 Version' },
-      { value: 'ALL_CSUopv2020_last_v', label: 'CSU-MLP GEFS Operational - 2020 Version' },
+      { value: 'ALL_CSUopUFVSv2022', label: 'CSU-MLP GEFS Operational - 2022 UFVS Version' },
+      { value: 'ALL_CSUopv2022', label: 'CSU-MLP GEFS Operational - 2022 Version' },
+      { value: 'ALL_CSUopv2020', label: 'CSU-MLP GEFS Operational - 2020 Version' },
     ]
   },
   {
     label: 'Bulk Error Statistics',
     options: [
-      { value: 'ALL_issuancetime_BSandAuROC_v', label: 'BSS and AuROC by Issuance Time - Compared to Observations' },
-      { value: 'ALL_bulkBSaAuROC_v', label: 'BSS and AuROC - Compared to Practically Perfect' },
-      { value: 'ALL_CSUMLPaEROvsPP_CB_v', label: 'Contingency Bias - Compared to Practically Perfect' },
-      { value: 'ALL_CSUMLPaEROvsPP_CSI_v', label: 'Critical Success Index - Compared to Practically Perfect' },
-      { value: 'ALL_CSUMLPaEROvsPP_FAR_v', label: 'False Alarm Ratio - Compared to Practically Perfect' },
-      { value: 'ALL_CSUMLPaEROvsPP_HIT_v', label: 'Hit Rate - Compared to Practically Perfect' },
+      { value: 'ALL_issuancetime_BSandAuROC', label: 'BSS and AuROC by Issuance Time - Compared to Observations' },
+      { value: 'ALL_bulkBSaAuROC', label: 'BSS and AuROC - Compared to Practically Perfect' },
+      { value: 'ALL_CSUMLPaEROvsPP_CB', label: 'Contingency Bias - Compared to Practically Perfect' },
+      { value: 'ALL_CSUMLPaEROvsPP_CSI', label: 'Critical Success Index - Compared to Practically Perfect' },
+      { value: 'ALL_CSUMLPaEROvsPP_FAR', label: 'False Alarm Ratio - Compared to Practically Perfect' },
+      { value: 'ALL_CSUMLPaEROvsPP_HIT', label: 'Hit Rate - Compared to Practically Perfect' },
     ]
   },
   {
     label: 'Fractional Coverage',
     options: [
-      { value: 'ALL_ERO_bycat_v', label: 'ERO Bulk Probabilities by Threshold' },
-      { value: 'ALL_CSUMLP_bycat_v', label: 'CSU-MLP Bulk Probabilities by Threshold' },
+      { value: 'ALL_ERO_bycat', label: 'ERO Bulk Probabilities by Threshold' },
+      { value: 'ALL_CSUMLP_bycat', label: 'CSU-MLP Bulk Probabilities by Threshold' },
+    ]
+  },
+  {
+    label: 'Heat Maps',
+    options: [
+      { value: 'ALL_ERO_spatprob_MRGL', label: '1-Year ERO - Marginal Frequency' },
+      { value: 'ALL_PP_spatprob_MRGL', label: '1-Year Practically Perfect - Marginal Frequency' },
+      { value: 'ALL_CSUopUFVSv2022_spatprob_MRGL', label: '1-Year 2022 UFVS CSU-MLP Operational GEFS - Marginal Frequency' },
+      { value: 'ALL_CSUopv2022_spatprob_MRGL', label: '1-Year 2022 CSU-MLP Operational GEFS - Marginal Frequency' },
+      { value: 'ALL_CSUopv2020_spatprob_MRGL', label: '1-Year 2020 CSU-MLP Operational GEFS - Marginal Frequency' },
+      { value: 'ALL_ERO_spatprob_SLGT', label: '1-Year ERO - Slight Frequency' },
+      { value: 'ALL_PP_spatprob_SLGT', label: '1-Year Practically Perfect  - Slight Frequency' },
+      { value: 'ALL_CSUopUFVSv2022_spatprob_SLGT', label: '1-Year 2022 UFVS CSU-MLP Operational GEFS - Slight Frequency' },
+      { value: 'ALL_CSUopv2022_spatprob_SLGT', label: '1-Year 2022 CSU-MLP Operational GEFS - Slight Frequency' },
+      { value: 'ALL_CSUopv2020_spatprob_SLGT', label: '1-Year 2020 CSU-MLP Operational GEFS - Slight Frequency' },
+      { value: 'ALL_ERO_spatprob_MDT', label: '1-Year ERO - Moderate Frequency' },
+      { value: 'ALL_PP_spatprob_MDT', label: '1-Year Practically Perfect  - Moderate Frequency' },
+      { value: 'ALL_CSUopUFVSv2022_spatprob_MDT', label: '1-Year 2022 UFVS CSU-MLP Operational GEFS - Moderate Frequency' },
+      { value: 'ALL_CSUopv2020_spatprob_MDT', label: '1-Year 2020 CSU-MLP Operational GEFS - Moderate Frequency' },
+      { value: 'ALL_ERO_spatprob_HIGH', label: '1-Year ERO - High Frequency' },
+      { value: 'ALL_PP_spatprob_HIGH', label: '1-Year Practically Perfect  - High Frequency' },
+      { value: 'ALL_CSUopUFVSv2022_spatprob_HIGH', label: '1-Year 2022 UFVS CSU-MLP Operational GEFS - High Frequency' },
+      { value: 'ALL_CSUopv2022_spatprob_HIGH', label: '1-Year 2022 CSU-MLP Operational GEFS - High Frequency' },
+      { value: 'ALL_CSUopv2020_spatprob_HIGH', label: '1-Year 2020 CSU-MLP Operational GEFS - High Frequency' }
     ]
   }
 
 ];
+
+const archiveOptions = options.slice(0, 2)
 
 
 const ImageDisplay = (props) => {
@@ -52,7 +78,7 @@ const ImageDisplay = (props) => {
   const [selectedProduct, setSelectedProduct] = useState(options[0].options[0]);
   const [selectedDay, setSelectedDay] = useState(1);
   const [selectedArchiveDate, setSelectedArchiveDate] = useState((new Date()) - 1);
-  const [imgURL, setimgURL] = useState(baseURL + selectedProduct.value + 'day' + selectedDay.toString() + '.png')
+  const [imgURL, setimgURL] = useState('')
 
   const keyPressHandler = ({ key }) => {
     if (props.plotType === 'current') {
@@ -74,11 +100,46 @@ const ImageDisplay = (props) => {
   }
 
   const constructImgURL = () => {
+    let tmpURL = ''
+    if (props.plotType === 'current') {
+      
+      let tmpSelectedProduct = selectedProduct.value
+      if (tmpSelectedProduct === 'ALL_EROwST4gFFG' || tmpSelectedProduct === 'ALL_ST4gFFG' || tmpSelectedProduct === 'ALL_EROwALL_PP' ||
+        tmpSelectedProduct === 'ALL_CSUopUFVSv2022' || tmpSelectedProduct === 'ALL_CSUopv2022' || tmpSelectedProduct === 'ALL_CSUopv2020') {
 
+        tmpSelectedProduct = tmpSelectedProduct + '_last'
+      }
+
+      tmpURL = baseURL + tmpSelectedProduct + '_vday' + selectedDay.toString() + '.png'
+
+    } else {
+      let tmpStartDate = new Date(selectedArchiveDate)
+      let tmpEndDate = new Date(selectedArchiveDate)
+      tmpEndDate.setDate(tmpEndDate.getDate() + 1);
+
+      let startDateStr = tmpStartDate.toISOString().split('T')[0].replaceAll('-','')
+      let endDateStr = tmpEndDate.toISOString().split('T')[0].replaceAll('-','')
+
+      let tmpSelectedProduct = selectedProduct.value
+      let tmpvhr = '12'
+      if (tmpSelectedProduct === 'ALL_CSUopUFVSv2022' || tmpSelectedProduct === 'ALL_CSUopv2022' || tmpSelectedProduct === 'ALL_CSUopv2020') {
+          tmpSelectedProduct = tmpSelectedProduct + 'wALL'
+
+          if (selectedDay.toString() === '1') {
+            tmpvhr = '09'
+          } else {
+            tmpvhr = '00'
+          }
+          
+      }
+
+      tmpURL = baseURL + 'daybyday/' + tmpSelectedProduct + '_' + startDateStr + '12_to_' + endDateStr + '12_vday' + selectedDay.toString() + '_vhr' + tmpvhr + '.png'
+    }
+    return tmpURL
   }
 
   useEffect(() => {
-    constructImgURL()
+    setimgURL(constructImgURL())
   },[selectedDay, selectedProduct, selectedArchiveDate, props.plotType])
 
   useEffect(() => {
@@ -89,29 +150,63 @@ const ImageDisplay = (props) => {
     }
   },[selectedDay])
 
+  useEffect(() => {
+    console.log(selectedProduct.value, selectedDay)
+    if (!(selectedProduct.value === 'ALL_CSUopUFVSv2022' || selectedProduct.value === 'ALL_CSUopv2022') && selectedDay > 3) {
+      setSelectedDay(3)
+    }
+  },[selectedProduct])
+
   return(
     <div className={styles.ImageDisplayContainer}>
       <div className={styles.ProductSelectContainer}>
         <Select
           defaultValue={selectedProduct}
           onChange={setSelectedProduct}
-          options={options}
+          options={props.plotType === 'current' ? options : archiveOptions}
           placeholder={'Select Product'}
         />
       </div>
       {
         props.plotType === 'current' ?
         <div className={styles.DaySelectContainer}>
-          <button className={`${styles.daySelectButton} ${selectedDay === 1 ? styles.selected : ''}`} onClick={handleDayChange} value={1}>Day 1</button>
-          <button className={`${styles.daySelectButton} ${selectedDay === 2 ? styles.selected : ''}`} onClick={handleDayChange} value={2}>Day 2</button>
-          <button className={`${styles.daySelectButton} ${selectedDay === 3 ? styles.selected : ''}`} onClick={handleDayChange} value={3}>Day 3</button>
-          <button className={`${styles.daySelectButton} ${selectedDay === 4 ? styles.selected : ''}`} onClick={handleDayChange} value={4}>Day 4</button>
-          <button className={`${styles.daySelectButton} ${selectedDay === 5 ? styles.selected : ''}`} onClick={handleDayChange} value={5}>Day 5</button>
+          <button className={`${styles.DaySelectButton} ${selectedDay === 1 ? styles.selected : ''}`} onClick={handleDayChange} value={1}>Day 1</button>
+          <button className={`${styles.DaySelectButton} ${selectedDay === 2 ? styles.selected : ''}`} onClick={handleDayChange} value={2}>Day 2</button>
+          <button className={`${styles.DaySelectButton} ${selectedDay === 3 ? styles.selected : ''}`} onClick={handleDayChange} value={3}>Day 3</button>
+          { selectedProduct.value !== 'ALL_CSUopv2020' ? 
+            <>
+               <button className={`${styles.DaySelectButton} ${selectedDay === 4 ? styles.selected : ''}`} onClick={handleDayChange} value={4}>Day 4</button>
+               <button className={`${styles.DaySelectButton} ${selectedDay === 5 ? styles.selected : ''}`} onClick={handleDayChange} value={5}>Day 5</button>
+            </>
+          :
+            null
+          }
+          
         </div>
         :
-        <div>
-          <DatePicker selected={selectedArchiveDate} onChange={(date) => setSelectedArchiveDate(date)} />
-        </div>
+        <>
+          <div className={styles.DaySelectContainer}>
+            <button className={`${styles.DaySelectButton} ${selectedDay === 1 ? styles.selected : ''}`} onClick={handleDayChange} value={1}>Day 1</button>
+            <button className={`${styles.DaySelectButton} ${selectedDay === 2 ? styles.selected : ''}`} onClick={handleDayChange} value={2}>Day 2</button>
+            <button className={`${styles.DaySelectButton} ${selectedDay === 3 ? styles.selected : ''}`} onClick={handleDayChange} value={3}>Day 3</button>
+            { 
+              selectedProduct.value === 'ALL_CSUopUFVSv2022' || selectedProduct.value === 'ALL_CSUopv2022' ?
+              <>
+               <button className={`${styles.DaySelectButton} ${selectedDay === 4 ? styles.selected : ''}`} onClick={handleDayChange} value={4}>Day 4</button>
+               <button className={`${styles.DaySelectButton} ${selectedDay === 5 ? styles.selected : ''}`} onClick={handleDayChange} value={5}>Day 5</button>
+              </>
+              :
+              null
+            }
+          </div>
+          <div className={styles.ArchiveDatePickerContainer}>
+            <DatePicker 
+              className={styles.ArchiveDatePicker}
+              selected={selectedArchiveDate} 
+              onChange={(date) => setSelectedArchiveDate(date)} 
+              />
+          </div>
+        </>
       }
      
       <div className={styles.ImgContainer}>
